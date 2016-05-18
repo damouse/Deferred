6c4486ac4bec3b2cb93393a28cc0d8e4faab208a
damouse <damouse@damouses-Mac.local>
2016-05-17 19:08:22 -0500

    removed redundant constrain methods. Moving them to their own subclass soon

--- Deferred/AnyFunction.swift
+++ Deferred/AnyFunction.swift
@@ -5,6 +5,8 @@
 //  Created by damouse on 4/26/16.
 //
 //  Generic wrappers that allow "AnyFunction" to be accepted and constrained by type and number of parameters
+//  AnyFunction functionality seems to have been lost when this code was linked up with Convertible code. If we 
+//  still want to support that we have to change a few things up
 
 import Foundation
 
@@ -42,15 +44,11 @@ class BaseClosure<A, B>: AnyClosureType, ClosureType {
 }
 
 
-
 // These are factory functions. I'm not a fan of how they're set up right now, but its a work in progress.
 
-// This is a special case, since it also covers cases where either or both A and B can be Void. 
+// This is a special case, since it covers cases where either or both A and B can be Void.
 func constrain<A, B>(fn: A -> B)  -> BaseClosure<A, B> {
-    // return BaseClosure(fn: fn).setCurry { a in return [fn(try convert(a[0], to: A.self)) as! AnyObject]}
     return BaseClosure(fn: fn).setCurry { a in
-        // Strictly speaking this shouldnt be here
-        // Its a leftover from the generic overloading used in the Deferred class. It shouldn't affect funtionality
         let result: B?
         
         if A.self == Void.self {
@@ -67,17 +65,10 @@ func constrain<A, B>(fn: A -> B)  -> BaseClosure<A, B> {
     }
 }
 
-
 func constrain<A, B, C>(fn: (A, B) -> C) -> BaseClosure<(A, B), C> {
     return BaseClosure(fn: fn).setCurry { a in return [fn(try convert(a[0], to: A.self), try convert(a[1], to: B.self)) as! AnyObject]}
 }
 
-
-
-// Accepts any function
-func accept<A, B>(fn: A -> B)  -> BaseClosure<A, B> {
-    return BaseClosure(fn: fn)
-}
 
 
 
